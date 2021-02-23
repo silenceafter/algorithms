e1bebbdc121ec64645d24bf09355dfae80269954
Aleksandr Pinayev <a59-info@yandex.ru>
2021-02-23 18:44:44 +0300

    Задание 2

---
+++ lesson2/lesson2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace lesson2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          //
+        }
+
+        public static int StrangeSum(int[] inputArray)
+        {
+            int sum = 0;
+            for (int i = 0; i < inputArray.Length; i++) // O(N)
+            {
+                for (int j = 0; j < inputArray.Length; j++) // O(N)
+                {
+                    for (int k = 0; k < inputArray.Length; k++) // O(1+5+N) ~ O(N)
+                    {
+                        int y = 0;
+
+                        if (j != 0)
+                        {
+                            y = k / j; // O(1)
+                        }
+                        sum += inputArray[i] + i + k + j + y; // O(1)+O(1)+..+N = 5
+                    }
+                }
+            }
+            return sum; // O(N^3)
+        }
+    }
+}
