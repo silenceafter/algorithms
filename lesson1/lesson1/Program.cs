4bf42973a98cc0ed0a36db5c425e9112aecd7b79
1 <1>
2021-03-04 13:11:10 +0300

    Задание 1

---
+++ lesson1/lesson1/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace lesson1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PointClass pointOne = new PointClass();
+            pointOne.X = 5;
+            pointOne.Y = 10;
+            PointClass pointTwo = new PointClass();
+            pointTwo.X = 10;
+            pointTwo.Y = 15;
+
+            BenchmarkClass a = new BenchmarkClass();
+            Console.WriteLine(a.PointDistance(pointOne, pointTwo));
+            BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+        }
+    }
+
+    public class PointClass
+    { 
+        public float X;
+        public float Y; 
+    }
+
+    public struct PointStruct
+    {
+        public float X;
+        public float Y;
+
+        public float PointDistance(PointStruct pointOne, PointStruct pointTwo)
+        {
+            float x = pointOne.X - pointTwo.X;
+            float y = pointOne.Y - pointTwo.Y;
+            return MathF.Sqrt((x * x) + (y * y));
+        }
+    }
+
+    public class BenchmarkClass
+    {
+        [Benchmark]
+        public float PointDistance(PointClass pointOne, PointClass pointTwo)
+        {
+            float x = pointOne.X - pointTwo.X;
+            float y = pointOne.Y - pointTwo.Y;
+            return MathF.Sqrt((x * x) + (y * y));
+        }
+
+        //[Benchmark]
+        public void TestFloatPointClass()
+        {
+            
+        }
+
+        //[Benchmark]
+        public void TestSumBoxing()
+        {
+            object x = 99;
+            //SumRefType(x);
+        }
+    }
+}
