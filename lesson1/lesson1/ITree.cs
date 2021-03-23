8dedf68efed46591317fece6b5eac28ba09c1dec
Aleksandr Pinayev <a59-info@yandex.ru>
2021-03-23 13:21:31 +0300

    Задание 1 Обновление 23.03.21

---
+++ lesson1/lesson1/ITree.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lesson1
+{
+    public interface ITree
+    {
+        TreeNode GetRoot();
+        void AddItem(int value); // добавить узел
+        void RemoveItem(int value); // удалить узел по значению
+        TreeNode GetNodeByValue(int value); //получить узел дерева по значению
+        void PrintTree(); //вывести дерево в консоль
+    }
+}
---
+++ lesson1/lesson1/Test.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson1
+{
+    class Test
+    {
+        public int[] InputArray { get; set; }        
+        public string ExpectedArrayBFS { get; set; }
+        public string ExpectedArrayDFS { get; set; }
+    }
+}
