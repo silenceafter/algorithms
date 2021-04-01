82307a931b1800271cab99972b676824481c4ae6
Aleksandr Pinayev <a59-info@yandex.ru>
2021-04-01 11:07:53 +0300

    Задание 1 Обновление 01.04.21 2

--- lesson1/lesson1/Test.cs
+++ lesson1/lesson1/Test.cs
@@ -4,13 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace lesson2
+namespace lesson1
 {
     class Test
     {
         public int InputN { get; set; }
         public int InputM { get; set; }
-        public (int,int)[] InputMapArray { get; set; }
         public int Actual { get; set; }
+        public int Expected { get; set; }
     }
 }
