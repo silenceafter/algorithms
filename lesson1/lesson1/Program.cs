19d4e62f1357ef40052b7c49df60cc7044ebd3f4
Aleksandr Pinayev <a59-info@yandex.ru>
2021-04-01 08:05:21 +0300

    Задание 1 Обновление 01.04.21

---
+++ lesson1/lesson1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace lesson1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Test = new Test[5];
+            Test[0] = new Test()
+            {
+                InputN = 5,
+                InputM = 10, 
+                Actual = 715
+            };
+
+            Test[1] = new Test()
+            {
+                InputN = 10,
+                InputM = 5, 
+                Actual = 715
+            };
+
+            Test[2] = new Test()
+            {
+                InputN = 2,
+                InputM = 4, 
+                Actual = 4
+            };
+
+            Test[3] = new Test()
+            {
+                InputN = 4, 
+                InputM = 2, 
+                Actual = 4
+            };
+
+            Test[4] = new Test()
+            {
+                InputN = 7, 
+                InputM = 10, 
+                Actual = 5005
+            };
+
+            Task[] myTask = new Task[5];
+            for (int i = 0; i < myTask.GetLength(0); i++)
+            {
+                myTask[i] = new Task();
+                Console.WriteLine($"Test[{i}]");
+                Console.WriteLine($"InputArray = {Test[i].InputN}, {Test[i].InputM}");
+                Console.WriteLine($"Actual = {Test[i].Actual}");
+                myTask[i].FillArray(Test[i].InputN, Test[i].InputM, myTask[i].myArray);
+                Console.WriteLine("");
+            }
+        }
+    }
+}
---
+++ lesson1/lesson1/Test.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson1
+{
+    class Test
+    {
+        public int InputN { get; set; }
+        public int InputM { get; set; }
+        public int Actual { get; set; }
+    }
+}
